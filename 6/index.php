a635ae17919d5a409764f730dc2c0de6ed6ba00b
良成西元 <ryosei@Ryosei-MacBook-Pro.local>
2016-05-04 17:24:07 +0900

    提出用課題　修正版

--- 6/index.php
+++ 6/index.php
@@ -4,17 +4,14 @@
 <meta charset="UTF-8">
 <title>宿泊後のお掃除をマッチング</title>
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-<meta charset="utf-8">
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-
-
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
 <link href='https://fonts.googleapis.com/css?family=Seymour+One' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="anmeru.css" media="all">
-
 </head>
+
 <body>
-    <a href="index.html"><h5><img src="folder/logo2.png"></h5></a>
+    <a href="index.html"><h5><img src="folder/logo2.png" width="500px"></h5></a>
 <nav class="menu">
    <ul>
     <li><i class="fa fa-user"></i>
@@ -28,29 +25,24 @@
    </ul>
 </nav>
     
-
 <div id="content">
-    <h1 class="logo"> <i class="fa fa-pencil-square-o fa-2x"></i>評価</h1>
     
-        <form method="post" action="kakka2.php">
-            <p>日付:<input type="date" name="name" ></p>
-<p>評価:<select id="sweet" name="sweet" >
-				<option value="">▼お選びください</option>
-				<option value="★★★★★">★★★★★（5）</option>
-				<option value="★★★★☆">★★★★☆（4）</option>
-				<option value="★★★☆☆">★★★☆☆（3）</option>
-				<option value="★★☆☆☆">★★☆☆☆（2）</option>
-				<option value="★☆☆☆☆">★☆☆☆☆（1）</option>
-							</select>
-
-<p>コメント:<input type="text" name="tel" size="20"></p>
-<p><input type="submit" value="送信"></p>
-</form>
-    
-
-
+    <h2> a</h2>
+    <h1>ご感想をお聞かせください。</h1>
+    <form method="post" action="kakka2.php">
+         <p>日付:<input type="date" name="name" ></p>
+         <p>評価:<select id="sweet" name="sweet" >
+		<option value="">▼お選びください</option>
+		<option value="★★★★★">★★★★★（5）</option>
+		<option value="★★★★☆">★★★★☆（4）</option>
+		<option value="★★★☆☆">★★★☆☆（3）</option>
+		<option value="★★☆☆☆">★★☆☆☆（2）</option>
+	        <option value="★☆☆☆☆">★☆☆☆☆（1）</option>
+		</select>
+         <p>コメント:<textarea name="tel" rows="4" cols="40"></textarea></p> 
+<!--         <p>コメント:<input type="text" name="tel" ></p>-->
+         <p><button class="button8" type="submit">送信</button></p>
+    </form>
 </div>
-
-
 </body>
 </html>
--- 6/kakka2.php
+++ 6/kakka2.php
@@ -4,17 +4,13 @@
 <meta charset="UTF-8">
 <title>宿泊後のお掃除をマッチング</title>
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-<meta charset="utf-8">
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-
-
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
-<link href='https://fonts.googleapis.com/css?family=Seymour+One' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="anmeru.css" media="all">
-
 </head>
+
 <body>
-    <a href="index.html"><h5><img src="folder/logo2.png"></h5></a>
+    <a href="＃"><h5><img src="folder/logo2.png"></h5></a>
 <nav class="menu">
    <ul>
     <li><i class="fa fa-user"></i>
@@ -27,43 +23,42 @@
         <a href="#">ログイン</a></li>
    </ul>
 </nav>
+    
 <div id="main">
   <div id="contents">
-      <div id="hito"><img src="folder/hito.jpg" width="80%" >
-    </div>
-    <h3><i class="fa fa-calendar" aria-hidden="true"></i>
-スケジュール</h3>
-    <script async name='freecals' id='calinc-mem42679-height500' type="text/javascript" src="http://freecalend.com/jstac/calinc.js"></script>
-  <ul class="products">
-   <li>
-    <h3>クリーナー情報</h3><br>
-    <bt>３０代女性</bt><br>
-    <bt>クリーナー回数：３回</bt><br>
-    <bt>世田谷区</bt><br>
-    <i class="fa fa-trophy" aria-hidden="true"></i>
-  <bt>ANMERUランク</bt>
-    <i class="fa fa-star" aria-hidden="true"></i>
-    <i class="fa fa-star" aria-hidden="true"></i>
-    <i class="fa fa-star" aria-hidden="true"></i>
-    <i class="fa fa-star" aria-hidden="true"></i>
-    <i class="fa fa-star-o" aria-hidden="true"></i>
-   </li>
-<hr>
-   <h3>対応エリア</h3><br>
-     <bt>世田谷区、目黒区</bt><br>
-     <bt>通常清掃からリネンクリーニングまで</bt><br>
-<hr>
-    <li>
-      <h3>自己紹介</h3>
-      <bt>お掃除が得意な２児の母です！</bt><br>
-      <bt>少しでも自分の経験を社会に活かしたいと思い登録しました！</bt><bt></bt>
-      <bt>まずはお気軽にご相談ください！</bt>
-     </li>
-  </ul>
-    <hr>
-  <div id="kutikomi">
-    <h3>レビュー</h3>
-    <table>
+      <img src="folder/hito.jpg" >
+      <h3><i class="fa fa-calendar" aria-hidden="true"></i>スケジュール</h3>
+      <script async name='freecals' id='calinc-mem42679-height500' type="text/javascript" src="http://freecalend.com/jstac/calinc.js"></script>
+      <ul class="products">
+       <li>
+        <h3>クリーナー情報</h3><br>
+        <bt>３０代女性</bt><br>
+        <bt>クリーナー回数：３回</bt><br>
+        <bt>世田谷区</bt><br>
+        <i class="fa fa-trophy" aria-hidden="true"></i>
+        <bt>ANMERUランク</bt>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star-o" aria-hidden="true"></i>
+      </li>
+ <hr>
+     <h3>対応エリア</h3><br>
+       <bt>世田谷区、目黒区</bt><br>
+       <bt>通常清掃からリネンクリーニングまで</bt><br>
+ <hr>
+       <li>
+        <h3>自己紹介</h3>
+        <bt>お掃除が得意な２児の母です！</bt><br>
+        <bt>少しでも自分の経験を社会に活かしたいと思い登録しました！</bt><bt></bt>
+        <bt>まずはお気軽にご相談ください！</bt>
+       </li>
+      </ul>
+ <hr>
+<div id="kutikomi">
+  <h3>レビュー</h3>
+  <table>
 <?php
 $name = $_POST["name"];
 $mail = $_POST["sweet"];
@@ -98,15 +93,9 @@ foreach ($dataarray_reverse as $data) {
 
 
 ?>
-</table>
-      </div>
-<!--
-<div class="sample">
+  </table>
 </div>
--->
-</div><!-- #contents -->
-
-    
+</div>
 </div>
 </body>
 </html>
